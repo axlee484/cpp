9110cd08a3f408063e3241404553e95417ea41e5
ayush <singhayush484h@gmail.com>
2022-01-09 16:53:20 +0530

    Initialise cpp repo

---
+++ polymorphism.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class Animal
+{
+    int num;
+
+public:
+    Animal() : num(0) {}
+    virtual void getName() { std::cout << "Animal"; }
+    void hello() { getName(); }
+};
+
+class Dog : public Animal
+{
+    int num;
+
+public:
+    Dog() : num(1) {}
+    void getName() { std::cout << "Dog"; }
+    void dog_bark() { std::cout << "woof"; }
+};
+
+void caller(Animal &animal)
+{
+    animal.hello();
+}
+
+int main()
+{
+    Dog *animal = new Dog();
+    animal->hello();
+}
